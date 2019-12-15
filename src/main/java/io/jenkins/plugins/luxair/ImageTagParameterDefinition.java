bdfd068fe3efe2745dceecd45ada5560bd2b9cdd
Maduma <maduma@pt.lu>
2019-12-15 21:49:35 +0100

    using credential plugins

--- src/main/java/io/jenkins/plugins/luxair/ImageTagParameterDefinition.java
+++ src/main/java/io/jenkins/plugins/luxair/ImageTagParameterDefinition.java
@@ -1,13 +1,26 @@
 package io.jenkins.plugins.luxair;
 
+import com.cloudbees.plugins.credentials.CredentialsMatchers;
+import com.cloudbees.plugins.credentials.CredentialsProvider;
+import com.cloudbees.plugins.credentials.common.StandardListBoxModel;
+import com.cloudbees.plugins.credentials.common.StandardUsernameCredentials;
+import com.cloudbees.plugins.credentials.common.StandardUsernamePasswordCredentials;
+import com.cloudbees.plugins.credentials.domains.DomainRequirement;
 import hudson.Extension;
+import hudson.model.Item;
 import hudson.model.ParameterValue;
 import hudson.model.SimpleParameterDefinition;
 import hudson.model.StringParameterValue;
+import hudson.security.ACL;
+import hudson.util.ListBoxModel;
 import java.util.List;
+import java.util.logging.Level;
 import java.util.logging.Logger;
+import jenkins.model.Jenkins;
 import net.sf.json.JSONObject;
+import org.kohsuke.stapler.AncestorInPath;
 import org.kohsuke.stapler.DataBoundConstructor;
+import org.kohsuke.stapler.QueryParameter;
 import org.kohsuke.stapler.StaplerRequest;
 
 
@@ -19,10 +32,10 @@ public class ImageTagParameterDefinition extends SimpleParameterDefinition {
     private String image;
     private String registry;
     private String filter;
-    private String credentialid;
+    private String credentialId;
 
     @DataBoundConstructor
-    public ImageTagParameterDefinition(String name, String description, String image, String registry, String filter, String credentialid) {
+    public ImageTagParameterDefinition(String name, String description, String image, String registry, String filter, String credentialId) {
         super(name, description);
         this.image = image;
         this.registry = registry;
@@ -31,7 +44,7 @@ public class ImageTagParameterDefinition extends SimpleParameterDefinition {
         } else {
             this.filter = filter;
         }
-        this.credentialid = credentialid;
+        this.credentialId = credentialId;
     }
 
     public String getImage() {
@@ -50,17 +63,29 @@ public class ImageTagParameterDefinition extends SimpleParameterDefinition {
         return ".*";
     }
 
-    public String getCredentialid() {
-        return credentialid;
+    public String getCredentialId() {
+        return credentialId;
     }
 
     public List<String> getTags() {
         List<String> imageTags;
         String user = "";
         String password = "";
-        if(credentialid.split(":").length == 2) {
-            user = credentialid.split(":")[0];
-            password = credentialid.split(":")[1];
+
+        StandardUsernamePasswordCredentials c = CredentialsMatchers.firstOrNull(
+            CredentialsProvider.lookupCredentials(
+                StandardUsernamePasswordCredentials.class,
+                Jenkins.getInstanceOrNull().getItemGroup(),
+                ACL.SYSTEM,
+                new DomainRequirement()
+            ),
+            CredentialsMatchers.withId(credentialId)
+        );
+        if (c != null) {
+            user = c.getUsername();
+            password = c.getPassword().getPlainText();
+        } else {
+            logger.log(Level.INFO, "Cannot find credential for : " + credentialId);
         }
         imageTags = ImageTag.getTags(image, registry, filter, user, password);
         return imageTags;
@@ -85,6 +110,25 @@ public class ImageTagParameterDefinition extends SimpleParameterDefinition {
         public String getDisplayName() {
             return "Image Tag Parameter";
         }        
+
+        public ListBoxModel doFillCredentialIdItems(@AncestorInPath Item context,
+                                                    @QueryParameter String credentialId,
+                                                    @QueryParameter String remote) {
+            logger.log(Level.INFO, "context:" + context + ":");
+            logger.log(Level.INFO, "credentialId:" + credentialId + ":");
+            logger.log(Level.INFO, "remote:" + remote + ":");
+            logger.log(Level.INFO, "Auth:" + Jenkins.getAuthentication() + ":");
+            if (context == null && !Jenkins.get().hasPermission(Jenkins.ADMINISTER) ||
+                context != null && !context.hasPermission(Item.EXTENDED_READ)) {
+                logger.log(Level.INFO, "no permission to list credential");
+                return new StandardListBoxModel().includeCurrentValue(credentialId);
+            }
+            return new StandardListBoxModel()
+                    .includeEmptyValue()
+                    .includeAs(ACL.SYSTEM, context, StandardUsernameCredentials.class)
+                    .includeCurrentValue(credentialId);
+        }
+
     }
 
 }
